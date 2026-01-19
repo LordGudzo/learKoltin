0c601f9efa56dd4581192f4e0fcf05d32007fa05
LordGudzo <86236673+LordGudzo@users.noreply.github.com>
2026-01-19 13:09:58 +0200

    variableArithmetic.kt

--- basics.kt
+++ basics.kt
@@ -1,8 +1,15 @@
 import kotlin.math.pow
+const val MAX_USERS = 100  //const
+
 
 fun main() {
-    arithmetic(5,2)
-    variable()
+	variable()
+    arithmetic(5,2)    
+}
+
+fun variable(){ 
+	val name: String = "Andrey " // unmutable
+	var age: Int = 34 // mutable
 }
 
 fun arithmetic(a: Int, b: Int) {
@@ -12,22 +19,14 @@ fun arithmetic(a: Int, b: Int) {
 	val division = a / b
     val power = a.toDouble().pow(b)  // for Int not available .pow(), so first needs to convert to Double
 	val remainder = a % b
-	println("""
-        addition: $addition
+	println("""                      // """ raw string, allows to write multi-line text without escaping
+        addition: $addition			 // $ pastes the value of a variable directly to the String
         subtraction: $subtraction
         multiplication: $multiplication
         division: $division
         power: $power
         remainder: $remainder
-    """.trimIndent())
+    """.trimIndent())                 // .trimIndent() cleans spaces in the String
 }
 
-fun variable(){ 
-    val a: Int = 5
-    println(a.toDouble().pow(2))
-	  println("- Did Joffrey agree?\n- He did. He also said \"I love using \\n\".")  //Line Feed \n   
-    println("\"Khal Drogo's favorite word is \"athjahakar\"\"") // escape character \
-    println(70 * (3 + 4) / (8 + 2))
-    println(0.2 + 0.1)
-    println(Int.MAX_VALUE)
-}
+
